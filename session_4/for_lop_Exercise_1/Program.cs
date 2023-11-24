1f6c3998b4edaddb64dadb4fae454855606bb186
RamtinBeheshtAeen <ramtinba145822@gmail.com>
2023-11-24 10:46:00 +0330

    add Ex2 to session_4(for_loops)

--- session_4/for_lop_Exercise_1/Program.cs
+++ session_4/for_lop_Exercise_1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//////////////////////////////////////////////////////////////////////////
+// Author: Ramtin BeheshtAeen
+// Date: 24 Nov 2023
+// Session: Session 4
+// Content: for loop
+// Topic:  exercise2: plot the specific shape using nested for loops
+// Filename: Program.cs
+// Dependencies: - 
+//////////////////////////////////////////////////////////////////////////
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine();
+            for(int j = 0; j < 3; j++){
+                Console.Write("*");
+            }
+        }
+        Console.ReadLine();
+    }
+}
