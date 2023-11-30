191ee14b9dcfec0abad7e3b301b5f7e4fe874667
Ramtin_BeheshtAeen_Laptop <ramtinba145822@@gmail.com>
2023-11-30 13:44:19 -0600

    add class Exercise 2

---
+++ session_5/class_exercise_2/class_exercise_2/Program.cs
@@ -0,0 +1,41 @@
+﻿//////////////////////////////////////////////////////////////////////////
+// Author: Ramtin BeheshtAeen
+// Date: 30 Nov 2023
+// Session: Session 5
+// Content: Array
+// Topic:  Class exercise2
+// Filename: Program.cs
+// Dependencies: - 
+//////////////////////////////////////////////////////////////////////////
+internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        Console.WriteLine("Enter number of goods");
+        string[] goods = new string[int.Parse(Console.ReadLine())];
+        int[] price = new int[goods.Length];
+        int sum = 0;
+
+        for (int i = 0; i < goods.Length; i++)
+        {
+            Console.WriteLine("Enter the name of the current good:");
+            goods[i] = Console.ReadLine();
+
+            Console.WriteLine("Enter the price of the " + goods[i] + ":");
+            price[i] = int.Parse(Console.ReadLine());
+
+            sum += price[i];
+        }
+
+        Console.WriteLine("-----------------Jadval Mojodi---------------------");
+        for (int j = 0; j < goods.Length; j++)
+        {
+            Console.WriteLine("product_code: " + j + " product_name: " + goods[j]+ " product_price: "+ price[j]);
+        }
+
+        Console.WriteLine("Total sum: " + sum);
+
+
+
+    }
+}
